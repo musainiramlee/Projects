94ac01c6b24f13431260c31388d7f3c9bbd5a2ab
Musaini bin Ramlee <musaini.ramlee@tnb.com.my>
2020-05-22 09:10:13 +0800

    Simple application of unit conversion

--- Fuel Efficiency converter.py
+++ Fuel Efficiency converter.py
@@ -1,2 +1,11 @@
+eff = float(input("Please key in  your car's fuel efficiency "))
+unit = input("Please select 'L' if the unit is in L/100km or 'M' if the unit is in MPG")
 
-
+if unit.upper() == 'L':
+    converted = round((235.215/eff),2)
+    print(f"Your car's fuel efficiency equivalent to American units is " + f'{converted} MPG')
+elif unit.upper() == 'M':
+    converted = round((235.215/eff),2)
+    print(f"Your car's fuel efficiency equivalent to Canadian units is " + f'{converted} L/100km')
+else:
+    print("Please only select either 'L' or 'M'")
